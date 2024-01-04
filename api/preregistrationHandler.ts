d7d014fcb2a4d268125364ff6b3c48f445d2e7ab
venkatvi <vaidehi.venkatesan@gmail.com>
2024-01-04 06:07:48 +0000

    First try to create airtable record staticallly

--- api/preregistrationHandler.ts
+++ api/preregistrationHandler.ts
@@ -7,14 +7,51 @@ export default function handler(
     request: VercelRequest,
     response: VercelResponse,
   ) {
+    parent_1_name = "Arvind"
+    parent_1_phone = "123-456-7890"
+    parent_1_email = "abc@gmail.com"
+    parent_2_name = "Vaidehi"
+    parent_2_phone = "234-567-8901"
+    parent_2_email = "def@gmail.com"
+    child_name = "Keshav"
+    const reqBody = {
+      fields: {
+        "Parent 1 Name": parent_1_name,
+        "Parent 1 Phone": parent_1_phone,
+        "Parent 1 Email": parent_1_email,
+        "Parent 2 Name": parent_2_name,
+        "Parent 2 Phone": parent_2_phone,
+        "Parent 2 Email": parent_2_email,
+        "Child Name": child_name
+      }
+    }
+    const result = await createAirtableRecord(process.env, reqBody)
+    var recordCreationStatus = "Fail"
+    if (result.ok){ 
+      recordCreationStatus = "Pass"
+    }
     response.status(200).json({
       body: request.body,
       query: request.query,
       cookies: request.cookies,
-      "Testdata":"Test1"
+      "status": recordCreationStatus,
     });
   }
-
+export async function createAirtableRecord(env, body) {
+  try {
+    const result = fetch(`https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${encodeURIComponent(env.AIRTABLE_TABLE_NAME)}`, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        Authorization: `Bearer ${env.AIRTABLE_ACCESS_TOKEN}`,
+        'Content-Type': 'application/json', 
+      }
+    })
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+}
   /*
 async function submitHandler (request, env) {
   if (request.method !== "POST") {
