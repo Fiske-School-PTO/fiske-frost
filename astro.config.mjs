56c5d0ba6f3ea76ff2e17ebe45aafc262364ffaa
Jeff Kwan <jkwan@vistaprint.com>
2024-02-03 09:08:07 -0500

    Tighten up sponsors and add Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,22 @@ import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vercel from '@astrojs/vercel/serverless';
+import partytown from '@astrojs/partytown';
 
 // https://astro.build/config
 export default defineConfig({
-  server: { host: true},
+  server: { host: true },
   site: "https://www.fiskefrost.com/",
-  integrations: [tailwind(), mdx(), sitemap()],
+  integrations: [
+    tailwind(),
+    mdx(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
   output: 'server',
   adapter: vercel({
     imageService: true,
